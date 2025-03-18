bc836fcfa7ee2080ffeec90d020374a13e3bdb5e
Stephen Murphy <stephen@smurphydev.ie>
2025-03-18 16:36:05 +0000

    Update java config
    Updated java config for jdtl and lsp. Will need to add some extra
    functionalit later to enable debugging, but this is already a huge
    improvement.

--- lua/plugins/lsp.spec.lua
+++ lua/plugins/lsp.spec.lua
@@ -111,7 +111,66 @@ return {
 			nvim_lsp.jdtls.setup({
 				on_attach = on_attach,
 				capabilities = capabilities,
-				cmd = { "jdtls" },
+				cmd = {
+					"java",
+					"-Declipse.application=org.eclipse.jdt.ls.core.id1",
+					"-Dosgi.bundles.defaultStartLevel=4",
+					"-Declipse.product=org.eclipse.jdt.ls.core.product",
+					"-Dlog.protocol=true",
+					"-Dlog.level=INFO",
+					"-Xmx4g",
+					"--add-modules=ALL-SYSTEM",
+					"--add-opens', 'java.base/java.util=ALL-UNNAMED",
+					"--add-opens', 'java.base/java.lang=ALL-UNNAMED",
+				},
+				settings = {
+					java = {
+						format = {
+							settings = {
+								-- Use Google Java style guidelines for formatting
+								-- To use, make sure to download the file from https://github.com/google/styleguide/blob/gh-pages/eclipse-java-google-style.xml
+								-- and place it in the ~/.local/share/eclipse directory
+								url = "/.local/share/eclipse/eclipse-java-google-style.xml",
+								profile = "GoogleStyle",
+							},
+						},
+						signatureHelp = { enabled = true },
+						contentProvider = { preferred = "fernflower" }, -- Use fernflower to decompile library code
+						-- Specify any completion options
+						completion = {
+							favoriteStaticMembers = {
+								"org.junit.jupiter.api.Assertions.*",
+								"org.junit.jupiter.params.provider.Arguments.*",
+								"java.util.Objects.requireNonNull",
+								"java.util.Objects.requireNonNullElse",
+								"org.mockito.Mockito.*",
+							},
+							filteredTypes = {
+								"com.sun.*",
+								"java.awt.*",
+								"jdk.*",
+								"sun.*",
+							},
+						},
+						-- Specify any options for organizing imports
+						sources = {
+							organizeImports = {
+								starThreshold = 9999,
+								staticStarThreshold = 9999,
+							},
+						},
+						-- How code generation should act
+						codeGeneration = {
+							toString = {
+								template = "${object.className}{${member.name()}=${member.value}, ${otherMembers}}",
+							},
+							hashCodeEquals = {
+								useJava7Objects = true,
+							},
+							useBlocks = true,
+						},
+					},
+				},
 			})
 
 			-- Python language server config
